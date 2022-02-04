fc851e6229ba248df30bca2dd4ef4ed2c5727c97
Avishek verma <79629526+AvishekVerma@users.noreply.github.com>
2022-02-04 19:19:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, {setState } from 'react'
+import { signUpFormValidation } from './utils/validation.js'
 
 
 
 const App = () => {
+  const [state, setState] = useState({
+    'name':'',
+  'email':'',
+    'password':''
+  })
   
   return (
     <div>
@@ -14,7 +20,7 @@ const App = () => {
       <input id="password" type="text" /><br></br>
       <label>consent</label>
       <input id="consent" type="checkbox" /><br></br>
-      <button type='submit'>Signup</button>
+      <button type='submit' onClick={signUpFormValidation} >Signup</button>
     </div>
   )
 }
